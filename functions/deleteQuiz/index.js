6c093039f8c615df5a3cd825cbe2ea8db7cf9fc7
Martin <martin.kolberg@outlook.com>
2024-10-02 12:02:18 +0200

    Recent changes to delete operation

--- functions/deleteQuiz/index.js
+++ functions/deleteQuiz/index.js
@@ -1,17 +1,42 @@
-// deleteQuiz:
-//     handler: functions/deleteQuiz/index.handler
-//     events:
-//       - httpApi:
-//           path: /quiz/{quizId}
-//           method: DELETE
-
-import { sendError } from '../../helpers/responseHelper.js';
+import { getQuizById } from '../../helpers/quizHelper/quiz.js';
+import { tokenValidator } from '../../utils/auth.js';
+import { sendError, sendResponse } from '../../helpers/responseHelper.js';
+import middy from '@middy/core';
+import { db } from '../../db.js';
 
 const deleteQuiz = async (event, context) => {
   const { quizId } = event.pathParameters;
   const userId = event.userId;
 
   try {
+    const quizData = await getQuizById(quizId);
+    console.log('QUIZ-ID:', quizId);
+
+    console.log('QUIZ_DATA:', quizData);
+
+    if (!quizData) {
+      return sendError(404, `No quiz with quizId ${quizId} was found`);
+    }
+
+    if (quizData.userId !== userId) {
+      return sendError(403, 'Unauthorized to delete this quiz');
+    }
+
+    const deleteParams = {
+      TableName: 'quizTable',
+      Key: {
+        quizId: quizData.quizId,
+      },
+    };
+
+    await db.delete(deleteParams);
+
+    return sendResponse(200, {
+      message: 'Quiz deleted successfully',
+      quizId: quizId,
+      name: quizData.name,
+      deletedAt: new Date().toISOString(),
+    });
   } catch (error) {
     console.log(error.message);
     sendError(500, error.message);
--- functions/postQuiz/index.js
+++ functions/postQuiz/index.js
@@ -5,11 +5,13 @@ import { sendError, sendResponse } from '../../helpers/responseHelper.js';
 import { tokenValidator } from '../../utils/auth.js';
 
 const postQuiz = async (event, context) => {
-  console.log(event);
+  console.log('Event:', event);
   const userId = event.userId;
   const { name } = JSON.parse(event.body);
 
   const quizId = uuidv4();
+  const createdAt = new Date().toISOString();
+  console.log('Generated Quiz ID:', quizId);
 
   const params = {
     TableName: 'quizTable',
@@ -19,11 +21,25 @@ const postQuiz = async (event, context) => {
       userId,
       questions: [],
       type: 'quiz',
+      createdAt,
     },
   };
 
   try {
+    console.log('Checking for existing quiz...');
+    const existingQuiz = await db.get({
+      TableName: 'quizTable',
+      Key: { quizId },
+    });
+
+    if (existingQuiz.Item) {
+      console.log('Quiz already exists:', existingQuiz.Item);
+      return sendError(400, 'Quiz with this ID already exists');
+    }
+
+    console.log('Putting item into DB:', params);
     await db.put(params);
+    console.log('Item successfully put into DB');
     return sendResponse(200, params);
   } catch (error) {
     console.log(error.message);
